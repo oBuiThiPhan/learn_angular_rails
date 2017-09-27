96c51bcbec34fd993aa07d67884292e7046ecd61
phanbt58 <phanbt95@gmail.com>
2017-09-27 11:46:06 +0700

    Add CRUD recipe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,5 @@
 //= require angular-rails-templates
 //= require angular-resource/angular-resource
 //= require angular-rails-templates
+//= require angular-flash/dist/angular-flash
 //= require_tree .
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def index
     @recipes =
     if params[:keywords]
@@ -10,5 +12,48 @@ class RecipesController < ApplicationController
       format.html
       format.json{render json: @recipes.to_json}
     end
+  end
+
+  # def new
+  # end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json{render json: @recipe.to_json}
+    end
+  end
+
+  def create
+    @recipe = Recipe.new recipe_params
+    @recipe.save
+    respond_to do |format|
+      format.html {render 'show'}
+      format.json{render json: @recipe.to_json, status: 201}
+    end
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update_attributes recipe_params
+    respond_to do |format|
+      format.html
+      format.json{head :no_content}
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    respond_to do |format|
+      format.html
+      format.json{head :no_content}
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :instructions)
   end
 end
